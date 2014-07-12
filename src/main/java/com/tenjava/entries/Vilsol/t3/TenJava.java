c1bfff73628da685ccf838c4722949025ab76c95
Vilsol <vilsol2000@gmail.com>
2014-07-12 15:08:40 +0100

    Initial Commit, logging

--- src/main/java/com/tenjava/entries/Vilsol/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/Vilsol/t3/TenJava.java
@@ -1,5 +1,24 @@
 package com.tenjava.entries.Vilsol.t3;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+	
+	public static TenJava plugin;
+	public static Logger logger;
+	
+	public void onEnable(){
+		logger = this.getLogger();
+		plugin = this;
+		
+		
+		logger.info("Successfully Loaded!");
+	}
+	
+	public void onDisable(){
+		logger.info("Successfully Unloaded!");
+	}
+	
+}
